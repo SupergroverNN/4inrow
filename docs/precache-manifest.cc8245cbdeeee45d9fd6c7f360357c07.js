6977393cae6ab6bfffe3db7ca0134a2c8c9bbeb3
SupergroverNN <40541530+SupergroverNN@users.noreply.github.com>
2018-10-14 15:19:50 +0300

    Add files via upload

---
+++ docs/precache-manifest.cc8245cbdeeee45d9fd6c7f360357c07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fd83eba0b6b62cd3a99e",
+    "url": "/static/js/main.fd83eba0.chunk.js"
+  },
+  {
+    "revision": "7e802ebce41dd84e0e5e",
+    "url": "/static/js/1.7e802ebc.chunk.js"
+  },
+  {
+    "revision": "fd83eba0b6b62cd3a99e",
+    "url": "/static/css/main.5e4cb7b5.chunk.css"
+  },
+  {
+    "revision": "21a66a00266c5de1eb742ece88f3a0e7",
+    "url": "/index.html"
+  }
+];
